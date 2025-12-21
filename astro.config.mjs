b583b17b6f0356de0736fb3d55dc7a357f5e8cec
jarodlopez <127226881+jarodlopez@users.noreply.github.com>
2025-12-21 01:31:33 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react"; // 1. Importa React
 
 export default defineConfig({
-  integrations: [tailwind()],
+  // 2. Agrega react() a las integraciones
+  integrations: [tailwind(), react()],
+  
+  // 3. Forzar a Vite a reconocer Firebase
+  vite: {
+    optimizeDeps: {
+      include: ['firebase/app', 'firebase/firestore'],
+    },
+    ssr: {
+      external: ['firebase'],
+    }
+  }
 });
+
